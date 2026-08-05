59352a08c2547caed2a05d8b010390646c0f4414
Deepatharshan <panchdeepan147@gmail.com>
2026-08-05 14:50:07 +0530

    fix: update next.config.ts with new supabase project hostname for images

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -13,7 +13,7 @@ const nextConfig: NextConfig = {
       },
       {
         protocol: 'https',
-        hostname: 'lhekcdwaumyetewbdqzr.supabase.co',
+        hostname: 'vjilhfyiupdvtscmwxbt.supabase.co',
       },
     ],
   },
